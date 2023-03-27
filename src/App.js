6b1bdde809db925d18836239aab9eb193972118f
kevingillhouse <gillhousekevin@gmail.com>
2023-03-27 05:54:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Reservation from './pages/Reservation';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Auth from './pages/Auth';
+import Main from './components/Main';
+import CreateElement from './components/CRUD/create__element';
+import CreateCategory from './components/CRUD/create__category';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Main>
+            <Routes>
+              <Route exact path='/' element={< Menu />} />
+              <Route path='/home' element={< Home />} />
+              <Route path='/reservation' element={< Reservation />} />
+              <Route path='/gallery' element={< Gallery />} />
+              <Route path='/contact' element={< Contact />} />
+              <Route path='/auth' element={< Auth />} />
+              <Route path='/create-element' element={< CreateElement />} />
+              <Route path='/create-category' element={< CreateCategory />} />
+            </Routes>
+        </Main>
     </div>
   );
 }
